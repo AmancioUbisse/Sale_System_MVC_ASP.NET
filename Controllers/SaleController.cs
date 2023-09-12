4456ed314e3b1dee11ab3f3d7d34e5d37bc2aa74
Edwin Machaieie <machaieieedwin1@gmail.com>
2023-09-12 20:40:15 +0200

    Dashboard

---
+++ Controllers/SaleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Sale_System.Models;
+using System.Diagnostics;
+namespace Sale_System.Controllers
+{
+    public class SaleController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
